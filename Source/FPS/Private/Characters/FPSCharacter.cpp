2a655643f73c3a71fb45c30452af9cb511dbf880
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2026-07-11 19:30:02 -0400

    S1 L2 Create Shooter Character / Remove 1st Person Materials

---
+++ Source/FPS/Private/Characters/FPSCharacter.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/FPSCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AFPSCharacter::AFPSCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+#pragma region Camera Settings
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 0.f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 15.f;
+	SpringArm->bUsePawnControlRotation = true;
+	
+	FirstPersonCamera = CreateDefaultSubobject<UCameraComponent>("FirstPersonCamera");
+	FirstPersonCamera->SetupAttachment(SpringArm);
+	FirstPersonCamera->bUsePawnControlRotation = false;
+#pragma endregion 
+#pragma region 1st Person View Settings
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh1P");
+	Mesh1P->SetupAttachment(FirstPersonCamera);
+	Mesh1P->bOnlyOwnerSee = true;
+	Mesh1P->bOwnerNoSee = false;
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->bReceivesDecals = false;
+	Mesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh1P->PrimaryComponentTick.TickGroup = TG_PrePhysics;
+	
+	GetMesh()->bOnlyOwnerSee = false;
+	GetMesh()->bOwnerNoSee = true;
+	GetMesh()->bReceivesDecals = false;
+#pragma endregion
+}
+
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
