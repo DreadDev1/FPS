414f7b796cd72b2f10ae2de42bfce20f87d1da8f
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2026-07-11 22:25:39 -0400

    S1 L5 Create Shooter Player Controller

---
+++ Source/FPS/Private/Player/FPSPlayerController.cpp
@@ -0,0 +1,80 @@
+﻿#include "Player/FPSPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AFPSPlayerController::AFPSPlayerController()
+{
+	bReplicates = true;
+}
+
+void AFPSPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem)) { Subsystem->AddMappingContext(FPSIMC, 0); }
+}
+
+void AFPSPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* FPSInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	FPSInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+	FPSInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
+	FPSInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Input_Jump);
+	FPSInputComponent->BindAction(CrouchAction, ETriggerEvent::Started, this, &ThisClass::Input_Crouch);
+}
+
+void AFPSPlayerController::Input_Crouch()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	if (UCharacterMovementComponent* CMC = GetCharacter()->GetCharacterMovement(); IsValid(CMC))
+	{
+		CMC->bWantsToCrouch = !CMC->bWantsToCrouch;
+	}
+}
+
+void AFPSPlayerController::Input_Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+	UCharacterMovementComponent* CMC = GetCharacter()->GetCharacterMovement();
+	if (!IsValid(CMC)) return;
+	
+	if (CMC->bWantsToCrouch)
+	{
+		CMC->bWantsToCrouch = false;
+	}
+	else
+	{
+		GetCharacter()->Jump();
+	}
+}
+
+void AFPSPlayerController::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	if (APawn* ControlledPawn = GetPawn())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void AFPSPlayerController::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	AddYawInput(InputAxisVector.X);
+	AddPitchInput(InputAxisVector.Y);
+}
---
+++ Source/FPS/Public/Player/FPSPlayerController.h
@@ -0,0 +1,48 @@
+﻿
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FPSPlayerController.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
+
+
+
+UCLASS()
+class FPS_API AFPSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AFPSPlayerController();
+	
+	protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+private:
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputMappingContext> FPSIMC;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> CrouchAction;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	void Input_Crouch();
+	void Input_Jump();
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+};
