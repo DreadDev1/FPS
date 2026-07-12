20e0092203041d65cea53293391aa67b659083c6
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2026-07-11 23:20:31 -0400

    S2 L9 Create Weapon Class

---
+++ Source/FPS/Private/Wepon/Weapon.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include "Wepon/Weapon.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	bNetUseOwnerRelevancy = true;
+	
+#pragma region Mesh Settings
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh1P");
+	Mesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh1P->bReceivesDecals = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetHiddenInGame(true);
+	SetRootComponent(Mesh1P);
+	
+	Mesh3P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh3P");
+	Mesh3P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh3P->bReceivesDecals = false;
+	Mesh3P->CastShadow = true;
+	Mesh3P->SetupAttachment(Mesh1P);
+	Mesh3P->SetHiddenInGame(true);
+#pragma endregion
+}
+
+USkeletalMeshComponent* AWeapon::GetMesh1P() const
+{
+	return Mesh1P;
+}
+
+USkeletalMeshComponent* AWeapon::GetMesh3P() const
+{
+	return Mesh3P;
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/FPS/Public/Wepon/Weapon.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+class USkeletalMeshComponent;
+UCLASS()
+class FPS_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+	
+	USkeletalMeshComponent* GetMesh1P() const;
+	USkeletalMeshComponent* GetMesh3P() const;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	// Weapon Mesh: 1st person view
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FPS|Weapon")
+	TObjectPtr<USkeletalMeshComponent> Mesh1P;
+	
+	// Weapon Mesh: 3rd person view
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FPS|Weapon")
+	TObjectPtr<USkeletalMeshComponent> Mesh3P;
+	
+private:
+	
+};
